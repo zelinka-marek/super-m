4dae2e3c4f1d1e3eb941d29805754cb2ccd731ec
Marek Zelinka <mzelinka17@gmail.com>
2023-12-10 10:07:07 +0100

    update layout

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -4,10 +4,10 @@ export default function AboutRoute() {
   return (
     <div className="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:items-center lg:gap-x-16">
       <div>
-        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+        <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">
           About SuperM
         </h1>
-        <p className="mt-6 text-lg/8 text-gray-600">
+        <p className="mt-2 text-base/7 text-gray-600">
           We started operations in 2020. We guarantee fresh produce. Save time
           by shopping on our app and we&apos;ll deliver the products right to
           your home. <em>We use Stripe to process your payment.</em>
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,14 +5,14 @@ export default function IndexRoute() {
   return (
     <div className="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:items-center lg:gap-x-16">
       <div>
-        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+        <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">
           Online shopping simplified
         </h1>
-        <p className="mt-6 text-lg/8 text-gray-600">
+        <p className="mt-2 text-base/7 text-gray-600">
           Order your groceries from <em>SuperM</em> with our easy to use app,
           and get your products delivered straight to your doorstep.
         </p>
-        <div className="mt-10">
+        <div className="mt-6">
           <Link
             to="/products"
             className="inline-block rounded-md bg-teal-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600"
